ceda3cab391782664536685005e1d00677a402e0
leost3 <leost3@gmail.com>
2025-05-07 15:28:03 -0400

    :recycle: modify port for 5432

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -7,7 +7,7 @@ const client = new Client({
   user: process.env.POSTGRES_USER || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'local_password',
   host: process.env.POSTGRES_HOST || 'localhost',
-  port: parseInt(process.env.POSTGRES_PORT || '5433'),
+  port: parseInt(process.env.POSTGRES_PORT || '5432'),
   database: process.env.POSTGRES_DB || 'postgres',
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 })
